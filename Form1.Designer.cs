d291fb5e4c414e6531b69401a0e7a2316d8b9bbd
huhuhahei666 <88595311@qq.com>
2025-07-02 09:40:42 +0800

    7-2

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WinForms_singleselection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            rad1_2 = new RadioButton();
+            lblQuestion = new Label();
+            rad1_3 = new RadioButton();
+            rad1_4 = new RadioButton();
+            rad1_1 = new RadioButton();
+            btnSubmit = new Button();
+            btnNext = new Button();
+            lblFeedback = new Label();
+            lblProgress = new Label();
+            lblScore = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 15F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(24, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 52);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            label1.Click += label1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(rad1_2);
+            groupBox1.Controls.Add(lblQuestion);
+            groupBox1.Controls.Add(rad1_3);
+            groupBox1.Controls.Add(rad1_4);
+            groupBox1.Controls.Add(rad1_1);
+            groupBox1.Font = new Font("Microsoft YaHei UI", 10.5F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(24, 100);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(998, 501);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // rad1_2
+            // 
+            rad1_2.AutoSize = true;
+            rad1_2.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rad1_2.Location = new Point(6, 216);
+            rad1_2.Name = "rad1_2";
+            rad1_2.Size = new Size(263, 46);
+            rad1_2.TabIndex = 6;
+            rad1_2.TabStop = true;
+            rad1_2.Text = "radioButton1";
+            rad1_2.UseVisualStyleBackColor = true;
+            rad1_2.CheckedChanged += rad1_2_CheckedChanged;
+            // 
+            // lblQuestion
+            // 
+            lblQuestion.AutoSize = true;
+            lblQuestion.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblQuestion.Location = new Point(6, 48);
+            lblQuestion.Name = "lblQuestion";
+            lblQuestion.Size = new Size(82, 42);
+            lblQuestion.TabIndex = 2;
+            lblQuestion.Text = "题目";
+            // 
+            // rad1_3
+            // 
+            rad1_3.AutoSize = true;
+            rad1_3.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rad1_3.Location = new Point(6, 306);
+            rad1_3.Name = "rad1_3";
+            rad1_3.Size = new Size(263, 46);
+            rad1_3.TabIndex = 5;
+            rad1_3.TabStop = true;
+            rad1_3.Text = "radioButton1";
+            rad1_3.UseVisualStyleBackColor = true;
+            rad1_3.CheckedChanged += rad1_3_CheckedChanged;
+            // 
+            // rad1_4
+            // 
+            rad1_4.AutoSize = true;
+            rad1_4.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rad1_4.Location = new Point(6, 392);
+            rad1_4.Name = "rad1_4";
+            rad1_4.Size = new Size(263, 46);
+            rad1_4.TabIndex = 4;
+            rad1_4.TabStop = true;
+            rad1_4.Text = "radioButton1";
+            rad1_4.UseVisualStyleBackColor = true;
+            rad1_4.CheckedChanged += rad1_4_CheckedChanged;
+            // 
+            // rad1_1
+            // 
+            rad1_1.AutoSize = true;
+            rad1_1.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rad1_1.Location = new Point(6, 126);
+            rad1_1.Name = "rad1_1";
+            rad1_1.Size = new Size(263, 46);
+            rad1_1.TabIndex = 2;
+            rad1_1.TabStop = true;
+            rad1_1.Text = "radioButton1";
+            rad1_1.UseVisualStyleBackColor = true;
+            rad1_1.CheckedChanged += rad1_CheckedChanged;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.BackColor = SystemColors.ActiveCaption;
+            btnSubmit.Font = new Font("Microsoft YaHei UI", 10.5F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSubmit.ForeColor = SystemColors.ButtonHighlight;
+            btnSubmit.Location = new Point(49, 654);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(119, 68);
+            btnSubmit.TabIndex = 2;
+            btnSubmit.Text = "提交";
+            btnSubmit.UseVisualStyleBackColor = false;
+            btnSubmit.Click += btnSubmit_Click;
+            // 
+            // btnNext
+            // 
+            btnNext.BackColor = SystemColors.ActiveCaption;
+            btnNext.Font = new Font("Microsoft YaHei UI", 10.5F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnNext.ForeColor = SystemColors.ControlLightLight;
+            btnNext.Location = new Point(588, 654);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(118, 68);
+            btnNext.TabIndex = 3;
+            btnNext.Text = "下一题";
+            btnNext.UseVisualStyleBackColor = false;
+            btnNext.Click += btnNext_Click;
+            // 
+            // lblFeedback
+            // 
+            lblFeedback.AutoSize = true;
+            lblFeedback.Font = new Font("Noto Sans SC", 10.5F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblFeedback.Location = new Point(1056, 119);
+            lblFeedback.Name = "lblFeedback";
+            lblFeedback.Size = new Size(103, 40);
+            lblFeedback.TabIndex = 4;
+            lblFeedback.Text = "label2";
+            lblFeedback.Click += lblFeedback_Click;
+            // 
+            // lblProgress
+            // 
+            lblProgress.AutoSize = true;
+            lblProgress.Font = new Font("Noto Sans SC", 10.5F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblProgress.Location = new Point(1056, 257);
+            lblProgress.Name = "lblProgress";
+            lblProgress.Size = new Size(103, 40);
+            lblProgress.TabIndex = 7;
+            lblProgress.Text = "label2";
+            // 
+            // lblScore
+            // 
+            lblScore.AutoSize = true;
+            lblScore.Font = new Font("Noto Sans SC", 10.5F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblScore.Location = new Point(1056, 369);
+            lblScore.Name = "lblScore";
+            lblScore.Size = new Size(73, 40);
+            lblScore.TabIndex = 8;
+            lblScore.Text = "得分";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 31F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1992, 921);
+            Controls.Add(lblScore);
+            Controls.Add(lblProgress);
+            Controls.Add(lblFeedback);
+            Controls.Add(btnNext);
+            Controls.Add(btnSubmit);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private RadioButton rad1_1;
+        private Label lblQuestion;
+        private RadioButton rad1_3;
+        private RadioButton rad1_4;
+        private RadioButton rad1_2;
+        private Button btnSubmit;
+        private Button btnNext;
+        private Label lblFeedback;
+        private Label lblProgress;
+        private Label lblScore;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+using System.Drawing.Text;
+
+namespace WinForms_singleselection
+{
+    public partial class Form1 : Form
+    {
+        private string[] questions =
+            {
+            "1.下列哪件事真的发生过？",
+            "2.下列哪件商品在古代直播间会被 “禁售”？",
+            "3.以下哪句话最可能是老板对员工的 PUA？",
+            "4.下列哪句最符合宋代文人的 “凡尔赛”？",
+            "5.以下哪条 “秦朝朋友圈” 最可能是假的？"
+            };
+        private string[][] options =
+        {
+                new string[] { "乾隆给英国使团送《孙子兵法》当国礼", "李白为杨贵妃写 “云想衣裳花想容”", "哥伦布航行时带了本《马可・波罗游记》", "以上全是真的" },
+                new string[] { "唐代直播间：波斯进口的琉璃瓶", "元代直播间：景德镇青花瓷茶具", "明代直播间：玉米炒虾仁套餐", "清代直播间：机器缝制的龙袍" },
+                new string[] { "秦始皇对修长城民工：“建成后给你们发‘小篆十级证书’”", "汉武帝对董仲舒：“独尊儒术可以，但得兼容百家哦”", "唐太宗对魏征：“爱卿谏言虽狠，但朕就喜欢直球”", "慈禧对维新派：“变法可以，祖宗之法不能变哦～”" },
+                new string[] { "“朕刚在汴京盖了个皇家花园，也就比西湖大一点”", "唉，今天殿试又拿了榜首，状元及第好烦啊", "新买的交子居然贬值了，心疼我那二两银子", "最近和李清照姐姐组队写诗，她总让我先" },
+                new string[] { "李斯：终于用小篆统一文字啦，再也不用看六国乱码了！", "某方士：帮秦始皇找仙丹中，海边蹲守 ing~", "蒙恬：刚修完长城，累成狗，求点赞换休假", "刘邦：今天斩白蛇创业，投资人项羽已打款" }
+            };
+        private int[] correctAnswers = { 3, 3, 0, 1, 3 }; // 正确答案索引(0-based)
+        private int currentQuestionIndex = 0;
+        private int score = 0;
+        private RadioButton[] optionRadios;
+        public Form1()
+        {
+            InitializeComponent();
+            optionRadios = new RadioButton[] { rad1_1, rad1_2, rad1_3, rad1_4 };
+            SetGreeting();
+            LoadQuestion(currentQuestionIndex);
+
+        }
+        private void SetGreeting()
+        {
+            int currentHour = DateTime.Now.Hour;
+            if (currentHour < 12)
+            {
+                label1.Text = "早上好，欢迎来到历史知识测试";
+            }
+            else if (currentHour < 18)
+            {
+                label1.Text = "下午好，欢迎来到历史知识测试";
+            }
+            else
+            {
+                label1.Text = "晚上好，欢迎来到历史知识测试";
+            }
+        }
+        private void LoadQuestion(int index)
+        {
+            if (index >= 0 && index < questions.Length)
+            {
+                // 设置题目文本
+                lblQuestion.Text = questions[index];
+
+                // 设置选项文本
+                for (int i = 0; i < 4; i++)
+                {
+                    optionRadios[i].Text = options[index][i];
+                    optionRadios[i].Checked = false;
+                }
+
+                // 更新进度标签
+                lblProgress.Text = $"问题 {index + 1}/{questions.Length}";
+
+                // 重置反馈标签
+                lblFeedback.Text = "";
+                lblFeedback.ForeColor = Color.Black;
+            }
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // 检查是否选择了选项
+            int selectedOption = -1;
+            for (int i = 0; i < 4; i++)
+            {
+                if (optionRadios[i].Checked)
+                {
+                    selectedOption = i;
+                    break;
+                }
+            }
+
+            if (selectedOption == -1)
+            {
+                MessageBox.Show("请选择一个选项!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // 检查答案
+            if (selectedOption == correctAnswers[currentQuestionIndex])
+            {
+                score++;
+                lblFeedback.Text = "回答正确!";
+                lblFeedback.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblFeedback.Text = $"回答错误! 正确答案是: {options[currentQuestionIndex][correctAnswers[currentQuestionIndex]]}";
+                lblFeedback.ForeColor = Color.Red;
+            }
+
+            // 更新分数显示
+            lblScore.Text = $"得分: {score}/{questions.Length}";
+
+            // 禁用提交按钮，启用下一题按钮
+            btnSubmit.Enabled = false;
+            btnNext.Enabled = true;
+        }
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // 移动到下一题
+            currentQuestionIndex++;
+
+            if (currentQuestionIndex < questions.Length)
+            {
+                LoadQuestion(currentQuestionIndex);
+                btnSubmit.Enabled = true;
+                btnNext.Enabled = false;
+            }
+            else
+            {
+                // 测验结束
+                MessageBox.Show($"测验结束! 你的最终得分是: {score}/{questions.Length}", "测验完成",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // 重置测验
+                currentQuestionIndex = 0;
+                score = 0;
+                LoadQuestion(currentQuestionIndex);
+                lblScore.Text = "得分: 0";
+                btnSubmit.Enabled = true;
+                btnNext.Enabled = false;
+            }
+        }
+
+
+
+
+        private void rad1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rad1_3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rad1_4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rad1_2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void lblFeedback_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
